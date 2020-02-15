6a5072044d1a1a9ef85f6c38c4003c028cf50ec1
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-15 11:08:11 -0500

    deleted comments

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const PORT = 8080;
+
+function handleRequest(request, response) {
+	response.end('It Works!! Path Hit: ' + request.url);
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+	console.log('Server listening on: http://localhost:' + PORT);
+});
